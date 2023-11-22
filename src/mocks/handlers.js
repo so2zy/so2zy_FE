fd1dd4c6d2ee0855b6fa6851af20945d9d7f6585
minseokiim <alstj991226@naver.com>
2023-11-23 02:38:43 +0900

    Feat: 데이터 구조 변경

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,64 +1,52 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('/api/roomId', () => {
-    //객실 타입
-    return HttpResponse.json([
-      {
-        id: 1,
-        name: 'Standard',
-        checkIn: '18:00',
-        checkOut: '13:00',
-        originalPrice: '100,000',
-        salePrice: '90,000',
-        roomPicture:
-          'https://yaimg.yanolja.com/v5/2023/11/14/10/640/65534a484efab3.93517110.jpg',
-      },
-      {
-        id: 2,
-        name: 'Deluxe',
-        checkIn: '18:00',
-        checkOut: '13:00',
-        originalPrice: '100,000',
-        salePrice: '90,000',
-        roomPicture:
-          'https://yaimg.yanolja.com/v5/2023/05/31/16/640/647779ee189cf0.19714641.jpg',
-      },
-      {
-        id: 3,
-        name: 'Standard',
-        checkIn: '18:00',
-        checkOut: '13:00',
-        originalPrice: '100,000',
-        salePrice: '90,000',
-        roomPicture:
-          'https://yaimg.yanolja.com/v5/2023/11/14/10/640/65534a484efab3.93517110.jpg',
-      },
-    ]);
-  }),
-  http.get(`/api/placeId`, () => {
+  http.get(`/accommodations/1`, () => {
     //숙소 타입
     return HttpResponse.json([
       {
-        id: 1,
-        name: '롯데호텔',
-        place: '서울특별시 송파구 올림픽로 300',
-        picture:
-          'https://yaimg.yanolja.com/v5/2022/10/31/12/1280/635fc0f6abccc1.66460254.jpg',
-      },
-      {
-        id: 2,
-        name: '강릉 어쩌고 호텔',
-        place: '강릉인데요',
-        picture:
-          'https://yaimg.yanolja.com/v5/2023/05/31/16/640/647779ee189cf0.19714641.jpg',
-      },
-      {
-        id: 3,
-        name: '제주도인디',
-        place: '제주',
-        picture:
-          'https://yaimg.yanolja.com/v5/2023/05/31/16/640/647779ee189cf0.19714641.jpg',
+        accommodationName: '롯데호텔', //string
+        latitude: 30.5, // number
+        longitude: 30.2, // number
+        addressCode: '경기도 고양시 일산동구', //string
+        phoneNumber: '01039966042', //string, 여기서의 phone number는 숙소 번호?
+        accommodationImageList: [
+          //호텔 사진 여러장
+          {
+            id: 1, //이미지 아이디
+            url: 'https://yaimg.yanolja.com/v5/2022/10/31/12/1280/635fc0f6abccc1.66460254.jpg', //이미지 url
+          },
+        ],
+        roomList: [
+          //객실 사진
+          {
+            id: 1,
+            type: 'deluxe',
+            price: 3000000,
+            capacity: 3,
+            maxCapacity: 4,
+            checkIn: '2023-11-16T01:44:54.366046',
+            checkOut: '2023-11-16T01:44:54.366046',
+            stock: 4,
+            roomImageList: [
+              {
+                id: 1,
+                url: 'https://yaimg.yanolja.com/v5/2023/11/14/10/640/65534a484efab3.93517110.jpg',
+              },
+            ],
+          },
+          {
+            id: 2,
+            type: 'premium',
+            price: 500000,
+            capacity: 2,
+            maxCapacity: 4,
+            checkIn: '2023-11-16T01:44:54.366046',
+            checkOut: '2023-11-16T01:44:54.366046',
+            stock: 0,
+            roomImageList: [],
+          },
+        ],
       },
     ]);
   }),
