eae495c636d0b3c8b73b816ec737679510f72dd2
minseokiim <alstj991226@naver.com>
2023-11-22 14:51:10 +0900

    Design: placeDetail 페이지 레이아웃

--- src/pages/placeDetail/components/PlaceDetail.tsx
+++ src/pages/placeDetail/components/PlaceDetail.tsx
@@ -6,52 +6,86 @@ import { theme } from '@styles/theme';
 import { FaStar } from 'react-icons/fa';
 import { MdPlace } from 'react-icons/md';
 // import { FaLocationArrow } from 'react-icons/fa';
+import { RiShoppingBagLine } from 'react-icons/ri';
 
 const PlaceDetail = () => {
   return (
     <>
-      <StyledContainer>
-        <StyledBar>
-          <StyledBefore />
-          <StyledTitle>롯데 호텔</StyledTitle>
-          <StyledSpan>
-            <StyledButton>11.19~11.20 1박</StyledButton>
-            <StyledButton>인원수 3명</StyledButton>
-          </StyledSpan>
-        </StyledBar>
-      </StyledContainer>
+      <StyledBar>
+        <StyledBefore />
+        <StyledTitle>롯데 호텔</StyledTitle>
+        <StyledSpan>
+          <StyledButton>11.19~11.20 1박</StyledButton>
+          <StyledButton>인원수 3명</StyledButton>
+        </StyledSpan>
+      </StyledBar>
 
-      <StyledContainer>
-        <StyledImg>호텔 이미지 들어갈 공간</StyledImg>
-      </StyledContainer>
+      <StyledImg>. </StyledImg>
 
-      <StyledContainer>
-        <StyledMainTitle>
-          롯데 호텔
-          <StyledStar
-          // className={isChecked ? 'checked' : 'unchecked'}
-          // onClick={() => {
-          //   setIsChecked((prev) => !prev);
-          //   setStarBtnClicked(!starBtnClicked);
-          // }}
-          />
-        </StyledMainTitle>
-        <StyledLocation>
-          숙소 위치 보기
-          <MdPlace />
-          {/* <FaLocationArrow /> */}
-        </StyledLocation>
-        <StyledDescription>서울특별시 송파구 올림픽로 300</StyledDescription>
-        <StyledLine />
-      </StyledContainer>
+      <StyledMainTitle>
+        롯데 호텔
+        <StyledStar
+        // className={isChecked ? 'checked' : 'unchecked'}
+        // onClick={() => {
+        //   setIsChecked((prev) => !prev);
+        //   setStarBtnClicked(!starBtnClicked);
+        // }}
+        />
+      </StyledMainTitle>
+      <StyledLocation>
+        {/* map api */}
+        숙소 위치 보기
+        <MdPlace />
+        {/* <FaLocationArrow /> */}
+      </StyledLocation>
+      <StyledDescription>서울특별시 송파구 올림픽로 300</StyledDescription>
+      <StyledLine />
 
-      <StyledContainer>
-        <StyledLocation>객실 선택</StyledLocation>
-        <StyledSubContainer>
-          {/* <StyledDetailImg>숙소 사진</StyledDetailImg>
-          <StyledDetailImg>STANDARD</StyledDetailImg> */}
-        </StyledSubContainer>
-      </StyledContainer>
+      <StyledSubCategory>객실 선택</StyledSubCategory>
+      <StyledSubContainer>
+        <StyledDetailImg>.</StyledDetailImg>
+        <StyledDetail>
+          <StyledWrapper>
+            <StyledRoomTitle>STANDARD</StyledRoomTitle>
+            <StyledBag />
+          </StyledWrapper>
+          <StyledRoomType>숙박</StyledRoomType>
+          <StyledRoomTime>체크인 18:00~체크아웃 13:00</StyledRoomTime>
+          <StyledRealPrice>100,000원</StyledRealPrice>
+          <StyledSalePrice>90,000원</StyledSalePrice>
+          <StyledReservationButton>예약하기</StyledReservationButton>
+        </StyledDetail>
+      </StyledSubContainer>
+
+      <StyledSubContainer>
+        <StyledDetailImg>.</StyledDetailImg>
+        <StyledDetail>
+          <StyledWrapper>
+            <StyledRoomTitle>STANDARD</StyledRoomTitle>
+            <StyledBag />
+          </StyledWrapper>
+          <StyledRoomType>숙박</StyledRoomType>
+          <StyledRoomTime>체크인 18:00~체크아웃 13:00</StyledRoomTime>
+          <StyledRealPrice>100,000원</StyledRealPrice>
+          <StyledSalePrice>90,000원</StyledSalePrice>
+          <StyledReservationButton>예약하기</StyledReservationButton>
+        </StyledDetail>
+      </StyledSubContainer>
+
+      <StyledSubContainer>
+        <StyledDetailImg>.</StyledDetailImg>
+        <StyledDetail>
+          <StyledWrapper>
+            <StyledRoomTitle>STANDARD</StyledRoomTitle>
+            <StyledBag />
+          </StyledWrapper>
+          <StyledRoomType>숙박</StyledRoomType>
+          <StyledRoomTime>체크인 18:00~체크아웃 13:00</StyledRoomTime>
+          <StyledRealPrice>100,000원</StyledRealPrice>
+          <StyledSalePrice>90,000원</StyledSalePrice>
+          <StyledReservationButton>예약하기</StyledReservationButton>
+        </StyledDetail>
+      </StyledSubContainer>
     </>
   );
 };
@@ -59,37 +93,34 @@ export default PlaceDetail;
 
 const StyledLine = styled.hr`
   color: ${theme.colors.gray3};
-  // margin: 1.5rem 4rem;
-`;
-
-const StyledContainer = styled.div`
-  width: 100%;
-  // margin: 0 auto;
+  margin: 1rem 0;
 `;
 
 const StyledSubContainer = styled.div`
-  // margin: 0 4rem;
-  height: 10rem;
-  border: 0.5px solid ${theme.colors.gray2};
+  margin: 1rem 0;
+  height: 12rem;
+  // border: 0.5px solid ${theme.colors.gray2};
   border-radius: 8px;
-  box-shadow: 1px 1px 1px ${theme.colors.gray2};
+  // box-shadow: 1px 1px 1px ${theme.colors.gray2};
+  display: flex;
 `;
 
+// 이미지 밑 이름
 const StyledMainTitle = styled.div`
   display: inline-block;
-  vertical-align: top;
   font-size: ${theme.fonts.subtitle3.fontSize};
   font-weight: ${theme.fonts.subtitle1.fontWeight};
-  // margin: 0 4rem;
   display: flex;
   align-items: center;
+  vertical-align: top;
 `;
 
 const StyledStar = styled(FaStar)`
   margin-left: auto;
   vertical-align: top;
-  font-size: ${theme.fonts.subtitle5.fontSize};
+  font-size: ${theme.fonts.subtitle4.fontSize};
   color: ${theme.colors.yellow};
+  cursor: pointer;
 
   // &.checked {
   //   color: ${theme.colors.yellow};
@@ -99,9 +130,9 @@ const StyledStar = styled(FaStar)`
   // }
 `;
 
+// 위치보기부분
 const StyledLocation = styled.div`
   width: 100%;
-  // margin: 0.5rem 4rem;
   text-align: start;
   color: ${theme.colors.navy};
   font-size: ${theme.fonts.subtitle5.fontSize};
@@ -115,6 +146,15 @@ const StyledLocation = styled.div`
   }
 `;
 
+const StyledSubCategory = styled.div`
+  width: 100%;
+  text-align: start;
+  color: ${theme.colors.navy};
+  font-size: ${theme.fonts.subtitle5.fontSize};
+  font-weight: ${theme.fonts.subtitle3.fontWeight};
+  display: block;
+`;
+
 const StyledDescription = styled.div`
   width: 100%;
   margin: 0.5rem 0;
@@ -125,14 +165,90 @@ const StyledDescription = styled.div`
   font-weight: ${theme.fonts.body.fontWeight};
 `;
 
-const StyledImg = styled.div`
+//호텔 이미지
+const StyledImg = styled.p`
   width: 45rem;
   height: 25rem;
   background-color: ${theme.colors.gray2};
   margin: 1rem auto;
-  border-radius: 4px;
+  border-radius: 8px;
+`;
 
-  //반응형은 나중에
+//객실 이미지
+const StyledDetailImg = styled.p`
+  width: 45%;
+  height: 100%;
+  background-color: ${theme.colors.gray2};
+  border-radius: 8px 0 0 8px;
+  display: inline-block;
+  box-shadow: 2px 2px 2px ${theme.colors.gray2};
+`;
+
+const StyledDetail = styled.div`
+  padding: 1rem 0 0 1rem;
+  width: 55%;
+  height: 100%;
+  background-color: ${theme.colors.gray3};
+  border-radius: 0 8px 8px 0;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 2px 2px 2px ${theme.colors.gray2};
+`;
+
+const StyledWrapper = styled.div`
+  font-size: ${theme.fonts.subtitle5.fontSize};
+  font-weight: ${theme.fonts.subtitle1.fontWeight};
+  display: flex;
+  align-items: center;
+  vertical-align: top;
+`;
+
+const StyledRoomTitle = styled.div`
+  padding: 0 0 2rem 0;
+  width: 100%;
+  display: inline-flex;
+  justify-content: space-between;
+`;
+
+const StyledBag = styled(RiShoppingBagLine)`
+  font-size: ${theme.fonts.subtitle4.fontSize};
+  color: ${theme.colors.navy};
+  cursor: pointer;
+`;
+
+const StyledRoomType = styled.div`
+  width: 100%;
+  margin-bottom: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledRoomTime = styled.div`
+  width: 100%;
+  margin-bottom: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledRealPrice = styled.div`
+  width: 100%;
+  margin-bottom: 0.5rem;
+  padding-right: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  text-decoration: line-through;
+`;
+
+const StyledSalePrice = styled.div`
+  width: 100%;
+  margin-bottom: 0.5rem;
+  padding-right: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 const StyledBar = styled.div`
@@ -143,10 +259,10 @@ const StyledBar = styled.div`
 
 const StyledBefore = styled(GrLinkPrevious)`
   vertical-align: top;
+  cursor: pointer;
 `;
 
 const StyledTitle = styled.span`
-  // flex: 1;
   margin-left: 18rem;
   text-align: center;
   vertical-align: top;
@@ -156,6 +272,7 @@ const StyledTitle = styled.span`
 
 const StyledSpan = styled.span`
   margin-left: auto;
+  margin-right: 0;
   vertical-align: top;
 `;
 
@@ -167,5 +284,26 @@ const StyledButton = styled.button`
   border: none;
   border-radius: 6px;
   height: 1.75rem;
+  padding-right: 0.25rem;
+  cursor: pointer;
+`;
+
+const StyledReservationButton = styled.button`
+  background-color: ${theme.colors.navy};
+  color: white;
+  border: none;
+  border-radius: 6px;
+  height: 1.75rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-left: auto;
+  margin-right: 0.75rem;
   padding: 0.25rem;
+
+  &:hover {
+    border: none;
+    background-color: ${theme.colors.gray2};
+  }
 `;
