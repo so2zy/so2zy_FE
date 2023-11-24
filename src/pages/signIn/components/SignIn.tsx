b112f9fb514cbea9a14d297ea9d248c54f328872
chaeminseok <minseok23491@naver.com>
2023-11-25 05:14:08 +0900

    feat:회원가입 기능 구현 완료

--- src/pages/signIn/components/SignIn.tsx
+++ src/pages/signIn/components/SignIn.tsx
@@ -2,17 +2,20 @@ import { theme } from '@styles/theme';
 import styled from 'styled-components';
 import MainTwoIcon from '@assets/mainLogoTwo.svg';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { emailState, pwState } from 'recoil/atom';
 export const SignIn: React.FC = () => {
+  const [email, setEmail] = useRecoilState(emailState);
+  const [pw, setPw] = useRecoilState(pwState);
+  // const [userName, setUserName] = useRecoilState(userNameState);
+  // const signInUrl = 'http://localhost:8080/v1/login';
   const navigate = useNavigate();
-  const handleLogoClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    navigate('/');
-  };
+  const handleLogIn = () => {};
   return (
     <StyledNoHeaderWrap>
       <StyledSignInContent>
         <StyledSignInLeft>
-          <StyledMainLogoTwo onClick={handleLogoClick}>
+          <StyledMainLogoTwo onClick={() => navigate('/')}>
             <img src={MainTwoIcon} />
           </StyledMainLogoTwo>
         </StyledSignInLeft>
@@ -23,16 +26,35 @@ export const SignIn: React.FC = () => {
           </StyledSignInGreeting>
           <StyledInputWrap>
             <div>Email</div>
-            <input placeholder="이메일을 입력해주세요" />
+            <input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              placeholder="이메일을 입력해주세요"
+            />
           </StyledInputWrap>
           <StyledInputWrap>
             <div>비밀번호</div>
-            <input placeholder="비밀번호를 입력해주세요" />
+            <input
+              value={pw}
+              onChange={(e) => {
+                setPw(e.target.value);
+              }}
+              placeholder="비밀번호를 입력해주세요"
+              type="password"
+            />
           </StyledInputWrap>
           <StyledSignInError>
             로그인 정보가 일치하지 않습니다.
           </StyledSignInError>
-          <StyledSignInButton>로그인</StyledSignInButton>
+          <StyledSignInButton onClick={handleLogIn} disabled={!email || !pw}>
+            로그인
+          </StyledSignInButton>
+          <StyledSignInGoSignUp>
+            <span>계정이 없으신가요?</span>
+            <span onClick={() => navigate('/signUp')}>회원가입</span>
+          </StyledSignInGoSignUp>
         </StyledSignInRight>
       </StyledSignInContent>
     </StyledNoHeaderWrap>
@@ -44,7 +66,7 @@ export const StyledNoHeaderWrap = styled.div`
   align-items: center;
   box-shadow: 4px 4px 4px ${theme.colors.gray2};
   width: 1080px;
-  height: calc(100vh - 10px);
+  height: 800px;
   margin: 0 auto;
   background-color: ${theme.colors.gray3};
   @media (max-width: 1080px) {
@@ -54,12 +76,15 @@ export const StyledNoHeaderWrap = styled.div`
 const StyledSignInContent = styled.div`
   display: flex;
   width: 70%;
-  height: 50%;
+  height: 30rem;
   margin: 0 auto;
   background-color: #f3f3f3;
   border: 1px solid #a8a8a8;
   border-radius: 20px;
   box-shadow: 4px 4px 4px ${theme.colors.gray2};
+  @media (max-width: 1080px) {
+    width: 100%;
+  }
 `;
 const StyledMainLogoTwo = styled.div`
   img {
@@ -119,4 +144,16 @@ const StyledSignInButton = styled.button`
   font-size: 1.4rem;
   font-weight: 900;
   border-radius: 10px;
+`;
+const StyledSignInGoSignUp = styled.div`
+  margin-top: 0.5rem;
+  text-align: center;
+  font-size: 0.8rem;
+  span:first-child {
+    margin-right: 0.3rem;
+    color: black;
+  }
+  span:last-child {
+    cursor: pointer;
+  }
 `;
--- src/pages/signUp/components/SignUp.tsx
+++ src/pages/signUp/components/SignUp.tsx
@@ -23,10 +23,37 @@ export const SignUp: React.FC = () => {
   const [isEmailDuplicated, setIsEmailDuplicated] = useState(true);
   const [isSignUp, setIsSignUp] = useState(false);
   const navigate = useNavigate();
+  const emailCheckUrl = 'http://43.202.50.38:8080/v1/members';
+  const signUpUrl = 'http://43.202.50.38:8080/v1/members/register';
+  // 이메일 주소
+
+  // 데이터 요청 함수
+  const checkEmail = async (email: string) => {
+    try {
+      const response = await axios.get(`${emailCheckUrl}/email/verify`, {
+        params: {
+          email: email,
+        },
+        headers: {
+          accept: '*/*',
+        },
+      });
+
+      if (response.status === 200) {
+        console.log(response);
+        setIsEmailDuplicated(false);
+      } else {
+        console.log('이메일 검증 실패');
+        setIsEmailDuplicated(true);
+      }
+    } catch (error) {
+      console.error('에러:', error);
+    }
+  };
   // const checkIdDuplication = async (email: string) => {
   //   try {
-  //     const response = await axios.post(
-  //       'https://fastcampus-chat.net/check/id',
+  //     const response = await axios.get(
+  //       'http://43.202.50.38:8080/v1/members/email/verify',
   //       { email },
   //       {
   //         headers: {
@@ -48,90 +75,81 @@ export const SignUp: React.FC = () => {
   //   }
   // };
   // 목업데이터 이메일 체크
-  const emailCheck = async ({ email }: { email: string }) => {
-    const response = await axios.post('/api/emailCheck', { email });
-    if (response.status === 200) {
-      console.log('emailCheck', response.data);
-      if (response.data.isDuplicate) {
-        setIsEmailDuplicated(true);
-        console.log('중복된 이메일이존재합니다');
-      } else {
-        setIsEmailDuplicated(false);
-        console.log('사용가능한 이메일');
-      }
-    } else {
-      console.error('Failed to emailcheck');
-    }
-  };
-  //목업데이터 회원가입
-  const join = async ({
-    email,
-    pw,
-    userName,
-  }: {
-    email: string;
-    pw: string;
-    userName: string;
-  }) => {
-    const response = await axios.post('/api/join', { email, pw, userName });
-    console.log(response);
-    if (response.status === 200) {
-      console.log('sign', response.data);
-      setIsSignUp(true);
-      console.log('User joined successfully');
-      navigate('/signIn');
-    } else {
-      console.error('Failed to join user');
-    }
-  };
-  //실제 회원가입
-  // const handleSignUp = async (
-  //   email: string,
-  //   pw: string,
-  //   userName: string,
-  // ): Promise<void> => {
-  //   if (isSignUp === false) {
-  //     try {
-  //       const response = await axios.post(
-  //         'https://fastcampus-chat.net/signup',
-  //         {
-  //           email,
-  //           pw,
-  //           userName,
-  //         },
-  //         {
-  //           headers: {
-  //             'Content-Type': 'application/json',
-  //             serverId: process.env.REACT_APP_SERVER_ID,
-  //           },
-  //         },
-  //       );
-  //       if (
-  //         response.status === 200 &&
-  //         response.data.message === 'User created'
-  //       ) {
-  //         try {
-  //           setIsSignUp(true);
-
-  //           navigate('/login');
-  //           console.log('가입에 성공했습니다.');
-  //         } catch (error) {
-  //           console.error('유저 데이터 업로드를 실패했습니다 : ', error);
-  //         }
-  //       } else {
-  //         console.error('회원가입에 실패했습니다 :', response.data.message);
-  //         window.alert(
-  //           '서버와의 연결이 불안정 합니다. 잠시후 다시 시도해 주세요.',
-  //         );
-  //       }
-  //     } catch (error) {
-  //       console.error('회원가입 중 서버와의 에러가 발생했습니다 :', error);
-  //       window.alert(
-  //         '서버와의 연결이 불안정 합니다.\n잠시후 다시 시도해 주세요.',
-  //       );
+  // const emailCheck = async ({ email }: { email: string }) => {
+  //   const response = await axios.post('/api/emailCheck', { email });
+  //   if (response.status === 200) {
+  //     console.log('emailCheck', response.data);
+  //     if (response.data.isDuplicate) {
+  //       setIsEmailDuplicated(true);
+  //       console.log('중복된 이메일이존재합니다');
+  //     } else {
+  //       setIsEmailDuplicated(false);
+  //       console.log('사용가능한 이메일');
   //     }
+  //   } else {
+  //     console.error('Failed to emailcheck');
   //   }
   // };
+  //목업데이터 회원가입
+  // const join = async ({
+  //   email,
+  //   pw,
+  //   userName,
+  // }: {
+  //   email: string;
+  //   pw: string;
+  //   userName: string;
+  // }) => {
+  //   const response = await axios.post('/api/join', { email, pw, userName });
+  //   console.log(response);
+  //   if (response.status === 200) {
+  //     console.log('sign', response.data);
+  //     setIsSignUp(true);
+  //     console.log('User joined successfully');
+  //     navigate('/signIn');
+  //   } else {
+  //     console.error('Failed to join user');
+  //   }
+  // };
+  const handleSignUp = async (
+    email: string,
+    pw: string,
+    userName: string,
+  ): Promise<void> => {
+    try {
+      const requestBody = {
+        email: email,
+        password: pw,
+        name: userName,
+      };
+      console.log(email, pw, userName);
+      const response = await axios.post(signUpUrl, requestBody, {
+        headers: { accept: '*/*' },
+      });
+      console.log('signup response', response);
+      if (response.status === 200) {
+        try {
+          console.log(response);
+          setIsSignUp(true);
+
+          navigate('/signIn');
+          console.log('가입에 성공했습니다.');
+        } catch (error) {
+          console.error('유저 데이터 업로드를 실패했습니다 : ', error);
+        }
+      } else {
+        console.error('회원가입에 실패했습니다 :', response.data.message);
+        window.alert(
+          '서버와의 연결이 불안정 합니다. 잠시후 다시 시도해 주세요.',
+        );
+      }
+    } catch (error) {
+      console.error('회원가입 중 서버와의 에러가 발생했습니다 :', error);
+      window.alert(
+        '서버와의 연결이 불안정 합니다.\n잠시후 다시 시도해 주세요.',
+      );
+    }
+  };
   const handleSignUpButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (emailCheckClicked === false) {
@@ -144,15 +162,16 @@ export const SignUp: React.FC = () => {
     ) {
       alert('입력정보를 다시확인해주세요');
     } else {
-      join({ email, pw, userName });
+      handleSignUp(email, pw, userName);
     }
   };
 
   const handleEmailCheck = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    emailCheck({ email });
-    setEmailCheckClicked(true);
-    // checkIdDuplication(email);
+    if (isIdentificationValid(email)) {
+      setEmailCheckClicked(true);
+      checkEmail(email);
+    }
   };
 
   const handleLogoClick = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -263,7 +282,7 @@ export const SignUp: React.FC = () => {
 export const StyledNoHeaderWrap = styled.div`
   box-shadow: 4px 4px 4px ${theme.colors.gray2};
   width: 1080px;
-  height: calc(100vh - 10px);
+  height: 800px;
   margin: 0 auto;
   background-color: ${theme.colors.gray3};
   @media (max-width: 1080px) {
@@ -310,6 +329,7 @@ const StyledSignUpInput = styled.input`
   border: none;
 `;
 const StyledSignUpButton = styled.button`
+  cursor: pointer;
   width: 100%;
   height: 3rem;
   background-color: ${theme.colors.navy};
