da770b4dbc12a146cd5319c917c6131f927cedbc
어승준 <eoseungjun7@naver.com>
2023-11-29 19:57:18 +0900

    Feat: 시군구별 중심좌표 구해서 지도 적용

--- src/pages/regionList/components/Map.tsx
+++ src/pages/regionList/components/Map.tsx
@@ -3,8 +3,8 @@ import { theme } from '@styles/theme';
 import { useEffect } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { regionListState, updateRegionListState } from '@recoil/regionList';
-import { useRecoilState } from 'recoil';
-import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { useState } from 'react';
 
 declare global {
   interface Window {
@@ -13,15 +13,27 @@ declare global {
 }
 
 const MapBox: React.FC = () => {
-  const latitude = 37.550091;
-  const longitude = 126.99295;
-  const [regionList, setRegionList] = useRecoilState(regionListState);
-  const navigate = useNavigate();
+  const [centerLat, setCenterLat] = useState(null);
+  const [centerLng, setCenterLng] = useState(null);
+
+  const addr = sessionStorage.getItem('selectedSigungu');
+  const geocoder = new window.kakao.maps.services.Geocoder();
+  geocoder?.addressSearch(addr, function (result: any, status: any) {
+    if (status === 'OK') {
+      setCenterLat(result[0].y);
+      setCenterLng(result[0].x);
+      console.log('해당 시군구의 중심좌표를 구했습니다!');
+    } else if (status === 'ZERO_RESULT') {
+      console.log('검색 결과가 없습니다.');
+    }
+  });
+
+  const regionList = useRecoilValue(regionListState);
 
   useEffect(() => {
     const container = document.getElementById(`map`);
     const options = {
-      center: new window.kakao.maps.LatLng(latitude, longitude),
+      center: new window.kakao.maps.LatLng(centerLat, centerLng),
       level: 8,
     };
 
@@ -42,43 +54,58 @@ const MapBox: React.FC = () => {
       });
 
       const contentString = ReactDOMServer.renderToString(
-        <a href={`/place/${positions[i].id}`}>
+        <div
+          style={{
+            backgroundColor: '#253C59',
+            borderRadius: '10px',
+            color: 'white',
+            cursor: 'pointer',
+            fontWeight: 'bold',
+            marginTop: '40px',
+            padding: '.5rem .75rem',
+          }}
+        >
           <div
             style={{
-              backgroundColor: '#253C59',
-              borderRadius: '10px',
-              color: 'white',
-              cursor: 'pointer',
-              fontWeight: 'bold',
-              marginTop: '40px',
-              padding: '.5rem .75rem',
+              display: 'flex',
+              gap: '.5rem',
+              marginBottom: '.5rem',
             }}
           >
+            <div style={{ paddingTop: '.25rem' }}>
+              <a
+                href={`/place/${positions[i].id}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {positions[i].name}
+              </a>
+            </div>
             <div
               style={{
                 display: 'flex',
-                gap: '.5rem',
-                marginBottom: '.5rem',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'white',
+                padding: '0 .2rem',
+                borderRadius: '.3rem',
+                color: 'black',
               }}
             >
-              <div style={{ paddingTop: '.25rem' }}>{positions[i].name}</div>
-              <div
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  backgroundColor: 'white',
-                  padding: '0 .2rem',
-                  borderRadius: '.3rem',
-                  color: 'black',
-                }}
-              >
-                x
-              </div>
+              x
             </div>
-            <div>{positions[i].price.toLocaleString('ko-KR')}원</div>
           </div>
-        </a>,
+          <div>
+            {positions[i].price.toLocaleString('ko-KR')}
+            <a
+              href={`/place/${positions[i].id}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              원
+            </a>
+          </div>
+        </div>,
       );
 
       const content = document.createElement('div');
@@ -101,7 +128,7 @@ const MapBox: React.FC = () => {
         });
       }
     }
-  }, [latitude, longitude]);
+  }, [centerLat, centerLng]);
 
   return (
     <div id="map" style={{ width: '40vw', height: '50vh', margin: '1rem 0' }} />
