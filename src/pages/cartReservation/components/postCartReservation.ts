93b3a222f421285209859b5398389bbc659ca348
jinjongsu <jjs9836@naver.com>
2023-12-07 00:03:56 +0900

    Feat: useMutation 사용

---
+++ src/pages/cartReservation/components/postCartReservation.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { PostPaymentProps } from '../CartReservation.page';
+
+export const postPayment = async (data: PostPaymentProps) => {
+  const accessToken = localStorage.getItem('accessToken');
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_SERVER}/v1/reservations`,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Token': accessToken,
+        },
+      },
+    );
+    if (res) {
+      return res.data;
+    }
+  } catch (error) {
+    console.error('결제 실패', error);
+  }
+};
